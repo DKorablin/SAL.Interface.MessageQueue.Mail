b6b772fd60e9540682749a23b1d4bdca310315ca
DKorablin2 <danila.korablin@unity3d.com>
2025-02-24 01:47:33 +0300

    Unify plugins build

--- SAL.Interface.MessageQueue.Mail/Properties/AssemblyInfo.cs
+++ SAL.Interface.MessageQueue.Mail/Properties/AssemblyInfo.cs
@@ -1,7 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: ComVisible(false)]
 [assembly: Guid("4bfc5ee2-9412-4676-a89f-f04d99491501")]
 [assembly: System.CLSCompliant(true)]
 
@@ -9,14 +8,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyMetadata("ProjectUrl", "https://github.com/DKorablin/SAL.Interface.MessageQueue.Mail")]
 #else
 
-[assembly: AssemblyTitle("Interface.MessageQueue.Mail")]
 [assembly: AssemblyDescription("Interfaces for mail Message Queue implementation")]
-#if DEBUG
-[assembly: AssemblyConfiguration("Debug")]
-#else
-[assembly: AssemblyConfiguration("Release")]
-#endif
-[assembly: AssemblyCompany("Danila Korablin")]
-[assembly: AssemblyProduct("Interface.MessageQueue.Mail")]
 [assembly: AssemblyCopyright("Copyright © Danila Korablin 2020")]
 #endif
